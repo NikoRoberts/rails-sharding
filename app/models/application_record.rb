5c10cd66462a5eda5d4382785ebe93025ad01709
Niko Roberts <niko@tasboa.com>
2021-05-14 21:03:06 +1000

    added sharding

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  connects_to shards: {
+    default: { writing: :primary, reading: :primary },
+    shard1: { writing: :shard1, reading: :shard1 }
+  }
 end
